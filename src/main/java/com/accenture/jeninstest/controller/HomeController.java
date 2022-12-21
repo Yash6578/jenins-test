0891bbfa54e5b1a67fb341a48bef02a6f6ec8963
yash6578 <yashchawla6578@gmail.com>
2022-12-21 23:52:29 +0530

    first commit

--- src/main/java/com/accenture/jeninstest/controller/HomeController.java
+++ src/main/java/com/accenture/jeninstest/controller/HomeController.java
@@ -1,2 +1,16 @@
-package com.accenture.jeninstest.controller;public class HomeController {
+package com.accenture.jeninstest.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class HomeController {
+    @GetMapping("/home")
+    ResponseEntity<Object> home() {
+        log.info("home method called");
+        return ResponseEntity.accepted().body("This is home page");
+    }
 }
